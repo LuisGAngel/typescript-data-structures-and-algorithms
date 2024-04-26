55241a8a9cd91802349871ba19bcaceb79e4a014
Luis Angel <luisgerardoangel@gmail.com>
2024-04-25 19:13:05 -0700

    Clean code

--- src/graph/types.ts
+++ src/graph/types.ts
@@ -1,10 +1,11 @@
-// A graph can be represented using an adjacency list
+// A directed/undirected unweighted graph can be represented using an adjacency list
 export type AdjacencyList<T> = Map<T, T[]>;
 
-// A graph can be represented using an adjacency matrix
+// A directed/undirected weighted/unweighted graph can be represented using an adjacency matrix but
+// the matrix doesn't contain the data that represents. e.g. value property
 export type AdjacencyMatrix = number[][];
 
-// A graph can be represented Nodes and Edges
+// A graph can be represented by Nodes and Edges
 export interface Node<T> {
     value: T;
     edges: Edge<T>[];
