bd13242c238dbcb32f5ce60db4ff52cb765b4f68
Luis Angel <luisgerardoangel@gmail.com>
2024-04-25 19:16:19 -0700

    Testing config changes

--- jest.config.ts
+++ jest.config.ts
@@ -103,7 +103,7 @@ const config: Config = {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    preset: "ts-jest",
+    // preset: "ts-jest",
 
     // Run tests from one or more projects
     // projects: undefined,
@@ -127,9 +127,7 @@ const config: Config = {
     // rootDir: undefined,
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
+    roots: ["dist"],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
